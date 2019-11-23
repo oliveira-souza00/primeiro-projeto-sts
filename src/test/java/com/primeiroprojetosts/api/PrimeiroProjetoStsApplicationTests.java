72cdc3ad352fa60e6ab5f605d1dd46b93017aa13
Francisco Jose Oliveira de Sousa <franciscooliveira@brq.com>
2019-11-23 08:40:55 -0300

    Arquivos Iniciais do Projeto

---
+++ src/test/java/com/primeiroprojetosts/api/PrimeiroProjetoStsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeiroprojetosts.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoStsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
